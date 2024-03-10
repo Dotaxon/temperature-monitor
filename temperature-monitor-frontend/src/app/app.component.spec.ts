3f91ada07b847f09b1d7599c4181432faa0ad346
Dotaxon <74875238+Dotaxon@users.noreply.github.com>
2024-03-10 20:16:35 +0100

    init

--- temperature-monitor-frontend/src/app/app.component.spec.ts
+++ temperature-monitor-frontend/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'temperature-monitoring-frontend' title`, () => {
+  it(`should have as title 'temperature-monitor-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('temperature-monitoring-frontend');
+    expect(app.title).toEqual('temperature-monitor-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, temperature-monitoring-frontend');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('temperature-monitor-frontend app is running!');
   });
 });
