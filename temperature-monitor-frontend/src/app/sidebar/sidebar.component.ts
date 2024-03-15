d893d09e8c143e1b7a69e9a6e59c703dde4d57d4
Dotaxon <74875238+Dotaxon@users.noreply.github.com>
2024-03-15 15:53:41 +0100

    implemented sidebar functionality

--- temperature-monitor-frontend/src/app/sidebar/sidebar.component.ts
+++ temperature-monitor-frontend/src/app/sidebar/sidebar.component.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {SidebarElement} from "../../interfaces/sidebarElement";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    RouterLink
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
-  onClose(){
+  isOpen: boolean = false;
 
+  sidebarElements : SidebarElement[] = [
+    { name: "Sensoren", path: "/sensors" },
+    { name: "Daten", path: "/data" }
+  ];
+
+  onOpen(): void {
+    console.log("onOpen")
+    this.isOpen = true;
+  }
+
+  onClose(): void {
+    this.isOpen = false;
   }
 
 }
---
+++ temperature-monitor-frontend/src/interfaces/sidebarElement.ts
@@ -0,0 +1,4 @@
+export interface SidebarElement {
+  name: string,
+  path: string
+}
