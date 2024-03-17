af3ade001d2999c19c8cf6731b9a8b35395146ae
Dotaxon <74875238+Dotaxon@users.noreply.github.com>
2024-03-17 16:53:29 +0100

    createTables function in DatabaseManger

---
+++ go-api/DataBaseManager.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var database *sql.DB
+
+func initDatabase() {
+	var db, err = sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	database = db
+	createTables()
+}
+
+func createTables() {
+	_, _ = database.Exec(`CREATE TABLE IF NOT EXISTS "dataEntry" (
+		"entryID"	INTEGER NOT NULL,
+		"time"	NUMERIC,
+		"temperature"	REAL,
+		"sensorID"	INTEGER,
+		"hourID"	INTEGER,
+		FOREIGN KEY("hourID") REFERENCES "hourCollection"("hourID"),
+		FOREIGN KEY("sensorID") REFERENCES "sensor"("sensorID"),
+		PRIMARY KEY("entryID")
+	);`)
+
+	_, _ = database.Exec(`CREATE TABLE IF NOT EXISTS "hourCollection" (
+		"hourID"	INTEGER NOT NULL,
+		"time"	NUMERIC,
+		"average"	REAL,
+		"dayID"	INTEGER,
+		PRIMARY KEY("hourID"),
+		FOREIGN KEY("dayID") REFERENCES "dayCollection"("dayID")
+	);`)
+
+	_, _ = database.Exec(`CREATE TABLE IF NOT EXISTS "dayCollection" (
+		"dayID"	INTEGER NOT NULL,
+		"day"	NUMERIC,
+		"average"	REAL,
+		"weekID"	INTEGER,
+		FOREIGN KEY("weekID") REFERENCES "weekCollection"("weekID"),
+		PRIMARY KEY("dayID")
+	);`)
+
+	_, _ = database.Exec(`CREATE TABLE IF NOT EXISTS "weekCollection" (
+		"weekID"	INTEGER NOT NULL,
+		"startDay"	NUMERIC,
+		"average"	REAL,
+		PRIMARY KEY("weekID")
+	);`)
+
+	_, _ = database.Exec(`CREATE TABLE IF NOT EXISTS"sensor" (
+		"sensorID"	INTEGER NOT NULL,
+		"name"	TEXT,
+		PRIMARY KEY("sensorID")
+	);`)
+
+	fmt.Println("Created tables if necessary")
+}
--- go-api/main.go
+++ go-api/main.go
@@ -7,10 +7,13 @@ import (
 const BindingAddr = "localhost:3000"
 
 func main() {
+	initDatabase()
+
 	var router *gin.Engine = gin.Default()
 	router.GET("/test/data", getTestData)
 	router.GET("/test/:id", getTestId)
 	router.POST("/test/data", addTest)
 	router.POST("/test/sensor", addTestSensor)
 	router.Run(BindingAddr)
+
 }
